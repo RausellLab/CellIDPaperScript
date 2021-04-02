12bff40abe25c3bf3c067541071c9a0b10c8a645
cortalak <akira.cortal@gmail.com>
2021-04-02 21:12:35 +0200

    clean sup note 2

--- Sup_Note2/R/2_SupFig2.R
+++ Sup_Note2/R/2_SupFig2.R
@@ -1,75 +1,4 @@
-set.seed(1)
-library(CellID)
-library(Seurat)
-library(splatter)
-library(scater)
-library(tidyverse)
-library(foreach)
-library(irlba)
-library(magrittr)
-library(ComplexHeatmap)
-library(FastKNN)
-library(cowplot)
-library(ggpubr)
-library(grid)
-library(gtable)
-library(pbmcapply)
-library(doMC)
-registerDoMC(cores =60)
-
-# Simulate Data Fig1 ------------------------------------------------------
-simulation <- function(x){
-    print(x)
-    params <- splatter::newSplatParams()
-    sim <- splatter::splatSimulate(params, nGenes = 5000, batchCells = 1000, group.prob = c(0.3, 0.15, 0.25, 0.2, 0.1), method = "groups", dropout.type = "experiment", dropout.mid = 3, de.facLoc = 2,seed=x)
-    sim <- sim[nexprs(sim,byrow = T)>=5,]
-    SingleCellExperiment::logcounts(sim) <- log1p(SingleCellExperiment::counts(sim))
-    sim <- scater::runPCA(sim,ncomponents = 10,feature_set = rownames(sim), scale_features = T)
-    sim <- CellID::RunMCA(sim,nmcs = 10)
-    correlation <- cor(reducedDim(sim,type = "PCA"),reducedDim(sim,"MCA"),method = "spearman")
-return(correlation)
-}
-
-
-sim100 <- pbmclapply(X = 1:100, FUN = simulation, mc.cores = 50)
-
-
-# Heatmap Correlation -----------------------------------------------------
-
-Reduce(cbind, lapply(sim100, function(x) diag(abs(x)))) %>% apply(1,sd)
-Reduce(cbind,lapply(sim100, function(x) diag(abs(x)))) %>% apply(1,median)
-Mediancor_Matrix <- apply(simplify2array(lapply(sim100,abs)), c(1,2), median)
-
-# Cells -------------------------------------------------------------------
-params <- newSplatParams()
-sim <- splatSimulate(params, nGenes = 5000, batchCells= 1000, group.prob = c(0.3, 0.15, 0.25, 0.2, 0.1), method = "groups", dropout.type = "experiment" ,dropout.mid= 3, de.facLoc= 2,seed = 1)
-logcounts(sim) <- log1p(counts(sim))
-sim <- sim[nexprs(sim, byrow = T)>=5]
-print(sum((logcounts(sim))==0)/(dim(logcounts(sim))[1]*dim(logcounts(sim))[2]))
-sim <- runPCA(sim,ncomponents = 10, subset_row = rownames(sim),scale = T)
-sim <- RunMCA(sim,nmcs = 10)
-sim_seurat <- as.Seurat(sim)
-sim_seurat@reductions$MCA@feature.loadings <- attributes(x = reducedDim(sim,"MCA"))$genesCoordinates
-sim_seurat@reductions$MCA@misc$mca.flag <- TRUE
-sim_seurat@reductions$PCA@cell.embeddings <- - sim_seurat@reductions$PCA@cell.embeddings
-
-ggPCA <- DimPlot(sim_seurat, reduction = "PCA",  pt.size = 0.8, cols ="gray") + ggpubr::theme_classic2()  + theme(legend.position="none", aspect.ratio = 1)
-ggMCA <- DimPlot(sim_seurat, reduction = "MCA",  pt.size = 0.8, cols ="gray") + ggpubr::theme_classic2() +theme(legend.position="none", aspect.ratio = 1) 
-ggMCA_gene_space <- Loadings(sim_seurat,"MCA") %>%  as.data.frame() %>% ggplot(aes(x= MCA_1, y=MCA_2)) + geom_point(size=0.1, shape = 4) + ggpubr::theme_classic2() + theme(legend.position = "none", aspect.ratio = 1)
-SupFig1A <- ggarrange(ggPCA, legend = "none", labels = "A", vjust = 12, font.label = list(size =18))
-SupFig1BC <- ggarrange(plotlist = list(ggMCA, ggMCA_gene_space), ncol = 1,legend = "none", labels = c("B", "C"), font.label = list(size =18))
-SupFig1ABC <- ggarrange(SupFig1A,SupFig1BC, font.label = list(size =18),legend = "none")
-SupFig1Dgb <- ComplexHeatmap::Heatmap(abs(Mediancor_Matrix),cluster_rows = F, cluster_columns = F, name = "Median\nSpearman\ncorrelation", 
-                                col = circlize::colorRamp2(c(0, 0.5, 1), c("#4575B4", "white", "#D73027")), 
-                                cell_fun = function(j, i, x, y, width, height, fill) {
-                                    grid.text(sprintf("%.2f", abs(Mediancor_Matrix)[i, j]), x, y, gp = gpar(fontsize = 11, fontface = "bold"))
-                                },border =T,rect_gp = gpar(col = "white", lwd = 1),column_names_rot = 45,row_names_gp = gpar(fontface = "bold"),column_names_gp = gpar(fontface = "bold"),heatmap_legend_param = list(legend_height = unit(6, "cm"),border = "black",grid_width = unit(1, "cm"),labels_gp = gpar(fontface = "bold"),title_position = "topcenter"))
-SupFig1D <- ggarrange(ggdraw(grid.grabExpr(draw(SupFig1Dgb)))+ theme(plot.margin = unit(c(1,0,0,1), "cm")), labels = "D",font.label = list(size =18))
-SupFig1 <- ggarrange(SupFig1ABC,SupFig1D)
-
-ggsave(filename = "../FinalFigure/SupFig1.pdf", plot = SupFig1, width = 12.5 ,height = 6)
-ggsave(filename = "../FinalFigure/SupFig1.png", plot = SupFig1, width = 12.5 ,height = 6, dpi = 600)
-
+source("R/0_library.R")
 # NEarest Neighbour fold change -----------------------
 CellID_Drop <- function(sim,Dim,k,x, distance){
     print(x)
@@ -174,21 +103,18 @@ AUranking <- AUCell::AUCell_buildRankings(exprMat = logcounts(sim))
 
 
 # One Cell ---------------------------------------------------------------
+
+# Boxplot function
 ggboxplot_drop <- function(x) {
-    ggboxplot(x, 
-              x = "bin", 
-              y = "logFC", 
-              color = "black",
-              fill ="zero",
-              outlier.shape = NA, 
-              add = "jitter",
-              add.params = list(size=0.1, color= "grey")) + 
+    ggplot(x, aes(x = bin, y = logFC, fill = zero)) +
+        geom_boxplot(outlier.shape = NA) +
+        geom_point(size = 0.1, color = "grey", position = position_jitterdodge()) + theme_pubr(border = FALSE)+ 
         geom_hline(yintercept = 0) + 
         labs(fill="") +
         theme(legend.key.size =  unit(0.5, "in"))
 }
 
-
+# Using MCA distances
 CellID_MCA  <- CellID_Drop(sim = sim, Dim = 10, k = 50, distance = mca_distance, x = 1)
 Dummy_MCA   <- Naive_Drop(sim, k = 50, distance = mca_distance, x = 1)
 AUCELL_MCA  <- AUC_Drop(sim = sim, k = 50, distance = mca_distance, x = 1)
@@ -196,7 +122,7 @@ MCA_cellID_one_cell <- ggboxplot_drop(CellID_MCA)
 MCA_dummy_one_cell  <- ggboxplot_drop(Dummy_MCA)
 MCA_aucell_one_cell <- ggboxplot_drop(AUCELL_MCA)
 
-
+# Using PCA distances
 CellID_PCA  <- CellID_Drop(sim = sim, Dim = 10, k = 50, x = 1, distance = pca_distance)
 Dummy_PCA   <- Naive_Drop(sim,x = 1, k= 50, distance = pca_distance)
 AUCELL_PCA  <- AUC_Drop(sim = sim, k=  50, x = 1, distance = pca_distance)
@@ -204,6 +130,7 @@ PCA_cellID_one_cell <- ggboxplot_drop(CellID_PCA)
 PCA_dummy_one_cell  <- ggboxplot_drop(Dummy_PCA)
 PCA_aucell_one_cell <- ggboxplot_drop(AUCELL_PCA)
 
+#Using Cosine distances
 CellID_Cosine    <- CellID_Drop(sim = sim, Dim = 10, k = 50, x = 1, distance = cosine_distance)
 Dummy_Cosine     <- Naive_Drop(sim,x = 1, k = 50, distance = cosine_distance)
 AUCELL_Cosine    <- AUC_Drop(sim = sim, k = 50, x = 1, distance = cosine_distance)
@@ -215,14 +142,14 @@ Cosine_aucell_one_cell   <- ggboxplot_drop(AUCELL_Cosine)
 correlation <- foreach(dist = list(mca_distance, pca_distance, cosine_distance), 
                        .final = function(x) setNames(x, c("mca", "pca", "cosine"))) %:% 
     foreach(func = list(CellID_Drop, AUC_Drop, Naive_Drop),
-                      .final = function(x) rownames_to_column(as.data.frame(t(as.data.frame(setNames(x, c("CellID", "AUC", "Naive"))))), var = "methods")) %:% 
+            .final = function(x) rownames_to_column(as.data.frame(t(as.data.frame(setNames(x, c("CellID", "AUC", "Naive"))))), var = "methods")) %:% 
     foreach(x = 1:1000,.combine = rbind, 
             .final = function(x) apply(x, 2, median)) %dopar% {
-    A <- func(sim = sim, Dim = 10, k = 50, x = x, distance = dist)
-    A$rank <- seq(1:nrow(A))
-    corel <- cor.test(-A$rank, A$logFC, method = "spearman",exact = T)
-    c(pval=corel$p.value, rho = round(corel$estimate, digits = 2))
-}
+                A <- func(sim = sim, Dim = 10, k = 50, x = x, distance = dist)
+                A$rank <- seq(1:nrow(A))
+                corel <- cor.test(-A$rank, A$logFC, method = "spearman",exact = T)
+                c(pval=corel$p.value, rho = round(corel$estimate, digits = 2))
+            }
 
 
 
@@ -234,15 +161,15 @@ write_csv(correlation$cosine, path = "figure/cor_cosine.csv")
 # AUcell Ranking ----------------------------------------------------------
 
 median_drop <- function(x){DF1 <- x %>% group_by(bin,data) %>% summarise(median_logFC=median(logFC))
-    DF1 <- DF1 %>% dplyr::mutate(zero="none") %>% select(bin,data,zero,median_logFC) %>% ungroup
-    DF2 <- x %>% group_by(bin,data,zero) %>% summarise(median_logFC=median(logFC)) %>% ungroup
-    DFall <- rbind(DF1,DF2)
+DF1 <- DF1 %>% dplyr::mutate(zero="none") %>% select(bin,data,zero,median_logFC) %>% ungroup
+DF2 <- x %>% group_by(bin,data,zero) %>% summarise(median_logFC=median(logFC)) %>% ungroup
+DFall <- rbind(DF1,DF2)
 return(DFall)
 }
 
 
 median_value <- foreach(dist = list(mca_distance, pca_distance, cosine_distance), 
-        .final = function(x) setNames(x, c("mca", "pca", "cosine"))) %:% foreach(func = list(CellID_Drop, AUC_Drop, Naive_Drop), .final = function(x) setNames(x, c("CellID", "AUC", "Naive"))) %:% 
+                        .final = function(x) setNames(x, c("mca", "pca", "cosine"))) %:% foreach(func = list(CellID_Drop, AUC_Drop, Naive_Drop), .final = function(x) setNames(x, c("CellID", "AUC", "Naive"))) %:% 
     foreach(x= 1:1000, .final = function(x) median_drop(data.table::rbindlist(x))) %dopar% {func(sim, Dim = 10, k = 50, x = x, distance = dist) %>% mutate(data=x)}
 
 
@@ -269,9 +196,9 @@ supFig2_PCA <- ggarrange(plotlist = list(PCA_cellID_one_cell,PCA_gg_median_celli
 supFig2_Cosine <- ggarrange(plotlist = list(Cosine_cellID_one_cell,Cosine_gg_median_cellid, Cosine_dummy_one_cell, Cosine_gg_median_dummy, Cosine_aucell_one_cell, Cosine_gg_median_aucell), ncol = 2, nrow = 3, common.legend = T, labels = c("A","B","C","D","E","F"))
 
 
-ggsave(plot = supFig2_MCA,filename = "../FinalFigure/supFig2_MCA.pdf", width = 12, height = 10)
-ggsave(plot = supFig2_PCA,filename = "../FinalFigure/supFig2_PCA.pdf", width = 12, height = 10)
-ggsave(plot = supFig2_Cosine,filename = "../FinalFigure/supFig2_Cosine.pdf", width = 12, height = 10)
-ggsave(plot = supFig2_MCA,filename = "../FinalFigure/supFig2_MCA.png", width = 12, height = 10, dpi = 600)
-ggsave(plot = supFig2_PCA,filename = "../FinalFigure/supFig2_PCA.png", width = 12, height = 10, dpi = 600)
-ggsave(plot = supFig2_Cosine,filename = "../FinalFigure/supFig2_Cosine.png", width = 12, height = 10, dpi = 600)
+
+# Save Pdf ----------------------------------------------------------------
+
+
+ggsave(plot = supFig2_MCA, filename = "../FinalFigure/supFig2.pdf", width = 12, height = 10)
+ggsave(plot = supFig2_MCA, filename = "../FinalFigure/supFig2.png", width = 12, height = 10, dpi = 600)
---
+++ Sup_Note2/R/3_PCABiplots.R
@@ -0,0 +1,38 @@
+source("R/0_library.R")
+# PCA Biplots-----------------------------------------------------------------
+
+simulation2 <- function(x){
+    params <- newSplatParams()
+    sim <- splatSimulate(params, nGenes = 5000, batchCells= 1000, group.prob = c(0.3, 0.15, 0.25, 0.2, 0.1), method = "groups", dropout.type = "experiment" ,dropout.mid= 3, de.facLoc= 2,seed = x)
+    sim <- sim[nexprs(sim, byrow = T)>=5]
+    sim <- normalize(sim)
+    sim <- as.Seurat(sim)
+    sim <- NormalizeData(sim)
+    sim <- RunMCA(sim, nmcs = 10)
+    test_matrix <- t(sim@assays$RNA@data)
+    test_matrix_centered <- scale(test_matrix, center = TRUE, scale = TRUE) 
+    X <- test_matrix_centered
+    I <- nrow(X)
+    J <- ncol(X)
+    Q <- diag(1/I,I)
+    Q_sqrt <- diag(sqrt(1/I),I)
+    Q_sqrt_inv <- diag(1/sqrt(1/I),I)
+    T <- (Q_sqrt) %*% X
+    T_SVD <- svd(T, nv = 10, nu = 10) # small matrix
+    D_sing <-diag(T_SVD$d[1:10])
+    D_sing_inv <-diag(1/T_SVD$d[1:10])
+    D_eign <-D_sing^2
+    D_eign_inv <-diag((1/((T_SVD$d^2)[1:10])))
+    D_eign_inv_sqrt <-diag(sqrt(1/((T_SVD$d^2)[1:10])))
+    U<-T_SVD$u 
+    V<-T_SVD$v 
+    Std_Row_coordiantes = diag(1/sqrt(1/I),I) %*% U
+    Ppal_Col_coordiantes = (V %*% D_sing) %>% set_colnames(paste0("pca_",1:10))
+    cor(sim@reductions$mca@feature.loadings, Ppal_Col_coordiantes)
+}
+
+gene_cor <- pbmclapply(X = 1:100, FUN = simulation2, mc.cores = 50)
+Reduce(cbind, lapply(gene_cor, function(x) diag(abs(x)))) %>% apply(1,sd)
+Reduce(cbind,lapply(gene_cor, function(x) diag(abs(x)))) %>% apply(1,median)
+Mediangenecor_Matrix <- apply(simplify2array(lapply(gene_cor,abs)), c(1,2), median)
+diag(Mediangenecor_Matrix)
